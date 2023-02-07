b7c20057f7c368715bb467f174a259d5565601c1
ermol272 <ermolinandrey1990>
2023-02-07 23:27:12 +0300

    Mesto

--- script/index.js
+++ script/index.js
@@ -3,12 +3,14 @@ const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
 const profileUserName = document.querySelector('.profile__username');
 const profileUserWork = document.querySelector('.profile__user-work');
-const popupUserName = popup.querySelector('.popup__username');
-const popupUserWork = popup.querySelector('.popup__user-work');
+const popupUserName = popup.querySelector('.popup__input_text_username');
+const popupUserWork = popup.querySelector('.popup__input_text_user-work');
 const popupForm = popup.querySelector('.popup__container');
 
 
 const handleOpenedButtonClick = () => {
+    popupUserName.value = profileUserName.textContent;
+    popupUserWork.value = profileUserWork.textContent;
     popup.classList.add('popup_opened');
 }
 const handleCloseButtonClick = () => {
